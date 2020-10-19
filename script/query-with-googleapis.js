9952c3409d422929c87009f4efcee3ac30e38c3b
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2020-10-19 10:27:24 +0200

    pros/cons for using crux-api over googleapis

--- script/query-with-googleapis.js
+++ script/query-with-googleapis.js
@@ -1,4 +1,4 @@
-// install googleapis: yarn add -D googleapis
+// install googleapis: yarn add googleapis
 // run: CRUX_KEY='...' node script/query-with-googleapis.js
 
 import { google } from 'googleapis'
@@ -8,3 +8,14 @@ const crux = google.chromeuxreport('v1')
 crux.records
   .queryRecord({ auth: key, requestBody: { origin: 'https://example.com' } })
   .then((r) => console.log(JSON.stringify(r.data, null, '  ')), console.error)
+
+// How is crux-api different from googleapis?
+// Pros:
+// + It is focused and lightweight 23.7 kB vs 75.7 MB (3200 times smaller)
+// + It is isomorphic, works in a browser and node.js;
+// + It handles 404 & 429 errors automatically;
+// + Documentation is easier to follow;
+// + It is easier to fork, contribute, and understand the code;
+// + It supports batch requests (since v1.1).
+// Cons:
+// - It's an extra dependency package, that's build not by Google
