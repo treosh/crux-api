0655336cbbfeda4462b8fdb8c213cd2bc7dc46da
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2026-02-09 18:54:00 +0100

    improve response times

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ const maxRetryTimeout = 60 * 1000 // 60s
  * @typedef {{ key: string, fetch?: function }} CreateOptions
  * @typedef {{ url?: string, origin?: string, formFactor?: FormFactor }} QueryRecordOptions
  * @typedef {'ALL_FORM_FACTORS' | 'PHONE' | 'DESKTOP' | 'TABLET'} FormFactor
+ * @typedef {{ percentiles: { p75: number } }} PercentileValue
  * @typedef {{ histogram: { start: number | string, end: number | string, density: number }[], percentiles: { p75: number | string } }} MetricValue
+ * @typedef {{ histogram: { start: string, end: string, density: number }[], percentiles: { p75: string } }} MetricStringValue
+ * @typedef {{ fractions: { desktop: number, phone: number, tablet: number } }} FormFactorFractionValue
+ * @typedef {{ fractions: { image: number, text: number } }} NavigationTypesFractionValue
+ * @typedef {{ fractions: { navigate: number, navigate_cache: number, back_forward: number, back_forward_cache: number, reload: number, restore: number, prerender: number } }} ResourceTypesFractionValue
  * @typedef {{ year: number, month: number, day: number }} MetricDate
  * @typedef {{ firstDate: MetricDate, lastDate: MetricDate }} CollectionPeriod
  * @typedef {{ error: { code: number, message: string, status: string } }} ErrorResponse
@@ -19,17 +24,24 @@ const maxRetryTimeout = 60 * 1000 // 60s
  *      metrics: {
  *        first_contentful_paint?: MetricValue,
  *        largest_contentful_paint?: MetricValue,
- *        first_input_delay?: MetricValue,
- *        cumulative_layout_shift?: MetricValue,
+ *        cumulative_layout_shift?: MetricStringValue,
  *        interaction_to_next_paint?: MetricValue,
  *        experimental_time_to_first_byte?: MetricValue,
- *      },
+ *        round_trip_time?: MetricValue,
+ *        form_factors?: FormFactorFractionValue,
+ *        navigation_types?: NavigationTypesFractionValue,
+ *        largest_contentful_paint_resource_type: ResourceTypesFractionValue,
+ *        largest_contentful_paint_image_time_to_first_byte: PercentileValue,
+ *        largest_contentful_paint_image_resource_load_delay: PercentileValue,
+ *        largest_contentful_paint_image_resource_load_duration: PercentileValue,
+ *        largest_contentful_paint_image_element_render_delay: PercentileValue,
+ *      }
  *      collectionPeriod: CollectionPeriod
  *    },
  *    urlNormalizationDetails?: {
  *      originalUrl: string,
  *      normalizedUrl: string
- *    }
+ *    },
  * }} SuccessResponse
  *
  * @typedef {(?number | string)[]} PercentileValues
@@ -39,6 +51,13 @@ const maxRetryTimeout = 60 * 1000 // 60s
  *    percentilesTimeseries: { p75s: PercentileValues }
  * }} HistoryValue
  *
+ * @typedef {number | 'NaN'} DensityValue
+ * @typedef {{ fractions: DensityValue[] }} Fractions
+ * @typedef {Object<string, Fractions>} FractionTimeseries
+ * @typedef {{ desktop: Fractions, phone: Fractions, tablet: Fractions }} FormFactorsFractionTimeseries
+ * @typedef {{ back_forward_cache: Fractions, prerender: Fractions, navigate: Fractions, navigate_cache: Fractions, reload: Fractions, restore: Fractions, back_forward: Fractions }} NavigationTypesFractionTimeseries
+ * @typedef {{ image: Fractions, text: Fractions }} ResourceTypesFractionTimeseries
+ * @typedef {{ percentilesTimeseries: { p75s: (?number)[] } }} HistoryPercentile
  * @typedef {{
  *    record: {
  *      key: {
@@ -47,12 +66,19 @@ const maxRetryTimeout = 60 * 1000 // 60s
  *        formFactor?: FormFactor
  *      },
  *      metrics: {
- *        first_input_delay?: HistoryValue,
  *        first_contentful_paint?: HistoryValue,
  *        largest_contentful_paint?: HistoryValue,
  *        cumulative_layout_shift?: HistoryValue,
  *        interaction_to_next_paint?: HistoryValue,
  *        experimental_time_to_first_byte?: HistoryValue,
+ *        round_trip_time?: HistoryValue,
+ *        form_factors?: { fractionTimeseries: FormFactorsFractionTimeseries },
+ *        navigation_types?: { fractionTimeseries: NavigationTypesFractionTimeseries },
+ *        largest_contentful_paint_resource_type: { fractionTimeseries: ResourceTypesFractionTimeseries },
+ *        largest_contentful_paint_image_time_to_first_byte: HistoryPercentile,
+ *        largest_contentful_paint_image_resource_load_delay: HistoryPercentile,
+ *        largest_contentful_paint_image_resource_load_duration: HistoryPercentile,
+ *        largest_contentful_paint_image_element_render_delay: HistoryPercentile,
  *      }
  *      collectionPeriods: CollectionPeriod[]
  *    },
